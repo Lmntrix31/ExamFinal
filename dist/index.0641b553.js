06173bfb3a91dbb5e7d1c462ebff33ff025d002e
Rossiny Longo <rossiny.longo@heig-vd.ch>
2023-02-07 14:51:00 +0100

    test

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -557,6 +557,16 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"bNKaB":[function(require,module,exports) {
+async function getData() {
+    try {
+        const response = await fetch(" https://randomuser.me/api/");
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+getData();
 
 },{}]},["lKzq4","bNKaB"], "bNKaB", "parcelRequirec56a")
 
