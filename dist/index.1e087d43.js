6d9024437c0880975cb4867ffd481faafd7a3d35
Rossiny Longo <rossiny.longo@heig-vd.ch>
2023-02-07 14:45:49 +0100

    Déploiement

---
+++ dist/index.1e087d43.js
@@ -0,0 +1,2 @@
+!async function(){try{const o=await fetch(" https://randomuser.me/api/"),t=await o.json();console.log(t)}catch(o){console.error("Error fetching data:",o)}}();
+//# sourceMappingURL=index.1e087d43.js.map
